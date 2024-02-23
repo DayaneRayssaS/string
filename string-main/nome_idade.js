d6cfff23fb2130c00444b36c819b4a8441f9a1d8
DayaneRayssaS <128699223+DayaneRayssaS@users.noreply.github.com>
2024-02-23 11:52:30 -0300

    Add files via upload

---
+++ string-main/nome_idade.js
@@ -0,0 +1,6 @@
+let nome = `Dayy`
+let idade = `17`
+let altura = `1,63`
+let numero = `17`
+console.log(`meu nome é ${nome} e tenho ${idade} anos de idade, com ${altura} de altura`)
+console.log(`${peso} de peso, ${altura} de altura, ficando ${peso + numero}`) 
---
+++ string-main/string2.js
@@ -0,0 +1,10 @@
+let nome = "Luan"
+let endereço = "Meu coração";
+let personalidade1 = 'estranha';
+let personalidade2 = 'LInda';
+let personalidade3 = "encomoda mto";
+console.log(`a pessoa é ${personalidade1}`);
+console.log(`a pessoa é ${personalidade2}`);
+console.log(`a pessoa me ${personalidade3}`);
+console.log(`o nome da pessoa é ${nome}`)
+console.log(`o endereço do Luan é ${endereço}`);
